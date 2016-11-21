fbb4f2033668ee61f46b6916852c4e0e7792b852
cyrusclarke <cyrusjclarke@gmail.com>
2016-11-21 19:21:18 +0100

    Create TimedInputHandler.cs

---
+++ TimedInputHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public interface TimedInputHandler : IEventSystemHandler {
+
+	void Update ();
+	
+//	void HandleTimeOutput ();
+
+}
